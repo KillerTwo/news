c53713f96bb7166999164528a9bfff18117ae6e8
KillerTwo <lwt2710@163.com>
2018-04-03 17:55:13 +0800

    Initial commit
    创建项目

---
+++ src/main/java/com/lwt/news/NewsApplication.java
@@ -0,0 +1,12 @@
+package com.lwt.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsApplication.class, args);
+	}
+}
